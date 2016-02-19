1760d5dd6181577df58849d2806d3fb8917a374b
Dan Stelljes <stell124@morris.umn.edu>
2016-02-18 20:01:17 -0600

    Generated app skeleton

---
+++ README.md
@@ -0,0 +1,9 @@
+Badass-o-matic student management-o-tron 9000
+===
+
+This is a thing that hopefully demonstrates how awesome
+[ngResource](https://docs.angularjs.org/api/ngResource) is.
+
+General steps to make this:
+
+1. `yo angular-fullstack`: Run the generator. Go cure cancer while `npm` runs.
---
+++ client/components/footer/footer.directive.js
@@ -0,0 +1,12 @@
+'use strict';
+
+angular.module('badassStudentManagerApp')
+  .directive('footer', function() {
+    return {
+      templateUrl: 'components/footer/footer.html',
+      restrict: 'E',
+      link: function(scope, element) {
+        element.addClass('footer');
+      }
+    };
+  });
